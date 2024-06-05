6d58b41a3dd6ae6f7e98a9fd25fb7078c4561fd6
23DPAPIMA <103630425+13DPAPIMA@users.noreply.github.com>
2024-06-05 18:57:38 +0300

    Welcome edited

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -39,7 +39,7 @@ defineProps({
                 <Link
                     v-if="canRegister"
                     :href="route('/projects/create')"
-                    class="mx-5 my-5 inline-flex items-center px-4 py-2 bg-red-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-200"
+                    class="mx-5 my-5 inline-flex items-center px-4 py-2 bg-red-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-600 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-200"
                 >Start Hiring</Link>
 
                 <Link
@@ -57,7 +57,7 @@ defineProps({
 
         <div class="max-w-full mx-auto p-6 lg:p-8">
             <div class="flex justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256">
+                <svg xmlns="http://www.w3.org/2000/svg" x="50px" y="0px" width="100" height="100" viewBox="0,0,256,256">
                     <g fill="#e61616" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
                         <g transform="scale(5.12,5.12)">
                             <path d="M20,4c-0.35684,0.00005 -0.68663,0.19023 -0.8654,0.49906c-0.17878,0.30883 -0.17946,0.68952 -0.00178,0.99899l23,40c0.18349,0.31948 0.52778,0.5122 0.89605,0.50158c0.36827,-0.01062 0.70088,-0.22285 0.86567,-0.55236l4.04688,-8.09375c0.74294,-1.48837 0.69781,-3.25311 -0.12305,-4.70117l-14.79883,-26.11719c-0.88668,-1.5648 -2.55006,-2.53516 -4.34961,-2.53516zM21.72852,6h6.94141c1.08245,0 2.07606,0.58029 2.60938,1.52148l14.79883,26.11719c0.49314,0.86994 0.52128,1.92663 0.07422,2.82227l-3.21484,6.42578zM15.99609,11c-0.35259,0.00145 -0.67836,0.18845 -0.85742,0.49219l-10,17c-0.18216,0.30915 -0.18474,0.6922 -0.00676,1.00378c0.17799,0.31158 0.50925,0.50392 0.86808,0.50403h21c0.36627,-0.00003 0.70321,-0.20028 0.87832,-0.52198c0.1751,-0.3217 0.16034,-0.71338 -0.03847,-1.02099l-11,-17c-0.18503,-0.28613 -0.50301,-0.45837 -0.84375,-0.45703zM16.04102,13.9043l9.12109,14.0957h-17.41406zM3,35c-0.35465,-0.00037 -0.68298,0.18711 -0.8629,0.49273c-0.17992,0.30563 -0.18453,0.68368 -0.0121,0.9936l5,9c0.17671,0.31745 0.51168,0.5141 0.875,0.51367h28c0.35465,0.00037 0.68298,-0.18711 0.8629,-0.49273c0.17992,-0.30563 0.18453,-0.68368 0.0121,-0.9936l-5,-9c-0.17671,-0.31745 -0.51168,-0.5141 -0.875,-0.51367zM4.70117,37h25.71094l3.88672,7h-25.71094z"></path>
@@ -66,17 +66,33 @@ defineProps({
                 </svg>
             </div>
 
-         
-            <div class="center">
-                <h1 class="max-md:hidden flex-end my-40 text-red-600 text-7xl font-semibold">You Found The Right Freelance Platform </h1>
-            </div>
-   
-            <div class="flex justify-end bg-white-600 w-full h-screen max-md:hidden">
-                <div class="w-1/2 bg-red-600 overflow-hidden">
 
-                <img src="/freelancer_photo.jpg" alt="Freelancer Image" class="max-w-full w-700px h-screen ">
+            <div class="flex justify-center content-center items-center bg-white-600 w-full h-screen max-xl:hidden">
+                <div class="text-center">
+                    <h1 class=" px-4 py-2 bg-red-600 max-md:hidden text-white text-8xl font-bold ">JobHunt</h1>
+                    <h1 class="max-md:hidden my-4 text-red-600 text-4xl font-semibold">You Found The Right Freelance Platform</h1>
+                    <h1 class="max-md:hidden my-4 text-red-600 text-4xl font-semibold">You will find the best high-paying projects on our platform</h1>
+                    <p class="max-md:hidden my-4 text-red-600 text-4xl font-semibold">The best opportunity for your career</p>
                 </div>
             </div>
+
+            
+            <div class="flex justify-between bg-white-600 w-full h-screen max-xl:hidden">
+    <div class="flex items-center w-1/2 h-full">
+        <div class="text-center w-full">
+            <h1 class="max-md:hidden text-red-600 text-8xl font-bold">№ 1 In The Freelance Niche</h1>
+            <br>
+            <br>
+            <h1 class="max-md:hidden my-4 text-red-600 text-4xl font-semibold">JobHunt - Easy Peasy Lemon Squeezy</h1>
+            <h1 class="max-md:hidden my-4 text-red-600 text-4xl font-semibold">JobHunt Will Provide Safe Payments</h1>
+            <p class="max-md:hidden my-4 text-red-600 text-4xl font-semibold">JobHunt will Help You to find Colleagues</p>
+            <p class="max-md:hidden my-4 text-red-600 text-4xl font-semibold">JobHunt eto polnaja imba</p>
+        </div>
+    </div>
+    <div class="w-1/2 overflow-hidden">
+        <img src="/freelancer_photo.jpg" alt="Freelancer Image" class="w-full h-full object-cover">
+    </div>
+</div>
 
 
             <div class="content-center my-10">
@@ -91,50 +107,54 @@ defineProps({
         </div>
 
 
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 bg-red-600">
             <div class="max-md:hidden flex w-full my-10 max-w-3xl mx-auto mt-400 scroll-smooth">
-                <h1 class="my-5 text-red-600 text-4xl font-semibold">Popular Services</h1>
+                <h1 class="my-5 text-white text-4xl font-semibold">Popular Services</h1>
             </div>
         </div>
-        <div class="flex flex-row content-center items-center justify-center max-md:flex-col">
+        <div class="flex flex-row content-center items-center justify-center max-md:flex-col bg-red-600">
             <div class="max-w-sm mx-2 mb-4 rounded overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105">
-                <div class=" px-6 py-4 bg-red-600 text-white">
+                <div class=" px-6 py-4 bg-white text-red-600">
                     <div class="font-bold text-xl mb-2">AI Artist</div>
-                    <p class="text-base">Over 100+ projects</p>
+                    <p class="text-base">Most skilled AI Artists and Prompt Engineers</p>
                 </div>
             </div>
             <div class="max-w-sm mx-2 mb-4 rounded overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105">
-                <div class="px-6 py-4 bg-red-600 text-white">
+                <div class="px-6 py-4 bg-white text-red-600">
                     <div class="font-bold text-xl mb-2">Logo Design</div>
                     <p class="text-base">More than 1000 specialists</p>
                 </div>
             </div>
             <div class="max-w-sm mx-2 mb-4 rounded overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105">
-                <div class="px-6 py-4 bg-red-600 text-white">
+                <div class="px-6 py-4 bg-white text-red-600">
                     <div class="font-bold text-xl mb-2">Word Press</div>
-                    <p class="text-base">Yes</p>
+                    <p class="text-base">The best Word Press Professionals</p>
                 </div>
             </div>
             <div class="max-w-sm mx-2 mb-4 rounded overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105">
-                <div class="px-6 py-4 bg-red-600 text-white">
+                <div class="px-6 py-4 bg-white text-red-600">
                     <div class="font-bold text-xl mb-2">Voice Over</div>
                     <p class="text-base">Best voice artists for Hollywood</p>
                 </div>
             </div>
             <div class="max-w-sm mx-2 mb-4 rounded overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105">
-                <div class=" px-6 py-4 bg-red-600 text-white">
+                <div class=" px-6 py-4 bg-white text-red-600">
                     <div class="font-bold text-xl mb-2">Video Editor</div>
                     <p class="text-base">The best quality on market</p>
                 </div>
             </div>
         </div>
         
-
+<br>
+<br>
+<br>
+<br>
+<br>
 
 
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                 <div class="max-md:hidden flex w-full my-10 max-w-3xl mx-auto mt-400 scroll-smooth">
-                    <h1 class="flex-end my-10 text-red-600 text-7xl font-semibold">Over 2000+ projects</h1>
+                    <h1 class="rounded-md flex-end my-10 text-white text-7xl font-semibold bg-red-600 py-3 px-3">Over 2000+ projects</h1>
                 </div>
 
                 <div class="max-md:hidden flex w-full my-10 max-w-3xl mx-auto mt-400">
@@ -146,11 +166,11 @@ defineProps({
                     </div>
 
                     <div className="max-md:hidden flex w-90 max-w-3xl my-10 mx-auto mt-400">
-                        <h1 className="flex-end my-10  text-red-600 text-7xl font-semibold my-10">1000+ professionals all around the globe</h1>
+                        <h1 className="flex-end my-10  text-white bg-red-600 text-7xl font-semibold my-10 rounded py-3 px-3 ">1000+ professionals all around the globe</h1>
                     </div>
                     
                     <div className="max-md:hidden flex my-10  w-90 max-w-3xl mx-auto mt-400">
-                        <h1 className=" flex-start text-red-600 text-7xl font-semibold my-10">5000+ potetial clients visit our platform daily</h1>
+                        <h1 className=" flex-start text-white text-7xl font-semibold my-10 py-3 px-3 rounded bg-red-600">5000+ potetial clients visit our platform daily</h1>
                     </div>
         
                     <div className="max-md:hidden flex my-10 w-90 max-w-3xl mx-auto mt-400">
