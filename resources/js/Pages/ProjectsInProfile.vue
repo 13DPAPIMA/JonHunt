986a0122ff383df770634a521cc9e038d9d40d61
23DPAPIMA <103630425+13DPAPIMA@users.noreply.github.com>
2024-11-03 17:27:49 +0200

    feat: updated projects page

--- resources/js/Pages/ProjectsInProfile.vue
+++ resources/js/Pages/ProjectsInProfile.vue
@@ -5,24 +5,30 @@
                 Your projects
             </h2>
         </template>
-        <div class="max-w-5xl mx-auto bg-white dark:bg-gray-800 shadow sm:rounded-lg p-6 sm:p-8 my-10">
-            <h2 class="text-2xl font-semibold text-gray-900 max-w-full">My Projects</h2>
-            <ul class="mt-4 space-y-4 max-w-full">
-                <ul v-if="projects !== undefined" class="py-12">
-                    <li v-for="project in projects" :key="project.id" class="border-b border-gray-200 pb-4">
-                        <h3 class="text-lg font-medium text-gray-900 overflow-hidden text-ellipsis whitespace-nowrap">{{ project.title }}</h3>
-                        <p class="text-sm text-gray-600 overflow-hidden text-ellipsis whitespace-nowrap">{{ project.description }}</p>
-                        <p class="overflow-hidden text-ellipsis whitespace-nowrap"><strong>Niche:</strong> {{ project.niche }}</p>
-                        <p class="overflow-hidden text-ellipsis whitespace-nowrap"><strong>Completion Date:</strong> {{ project.completion_date }}</p>
-                        <p class="overflow-hidden text-ellipsis whitespace-nowrap"><strong>Budget:</strong> ${{ project.budget }}</p>
-                        <div class="mt-2">
-                            <Link :href="route('projects.edit', project.id)" class="text-blue-600 hover:text-blue-800 mr-4">Edit</Link>
-                            <button @click="deleteProject(project.id)" class="text-red-600 hover:text-red-800">Delete</button>
-                        </div>
-                    </li>
-                </ul>
+        <div class="max-w-5xl mx-auto bg-white dark:bg-gray-800 shadow sm:rounded-lg p-6 sm:p-8 my-10 selection:bg-red-500 selection:text-white overflow-hidden">
+           <div class="flex justify-between items-center">
+               <h2 class="text-2xl font-semibold text-gray-900 max-w-full">My Projects</h2>
+               <Link :href="route('/projects/create')" class="btn text-lg text-white bg-red-600 "> + Create new project </Link>
+            </div>
+            
+            <div v-if="!projects || projects.length === 0" class="py-12">
+                <h1>You have no projects. Please create one.</h1>
+            </div>
+            <ul v-else class="mt-4 space-y-4 max-w-full py-12">
+                <li v-for="project in projects" :key="project.id" class="border-b border-gray-200 pb-4">
+                    <h3 class="text-lg font-medium text-gray-900 overflow-hidden text-ellipsis whitespace-nowrap">{{ project.title }}</h3>
+                    <p class="text-sm text-gray-600 overflow-hidden text-ellipsis whitespace-nowrap">{{ project.description }}</p>
+                    <p class="overflow-hidden text-ellipsis whitespace-nowrap"><strong>Niche:</strong> {{ project.niche }}</p>
+                    <p class="overflow-hidden text-ellipsis whitespace-nowrap"><strong>Completion Date:</strong> {{ project.completion_date }}</p>
+                    <p class="overflow-hidden text-ellipsis whitespace-nowrap"><strong>Budget:</strong> ${{ project.budget }}</p>
+                    <div class="flex justify-end mt-2">
+                        <Link :href="route('projects.edit', project.id)" class="btn bg-white text-blue-600 hover:text-blue-800 mr-4">Edit</Link>
+                        <button @click="deleteProject(project.id)" class="btn bg-red-600 rounded-lg text-white hover:bg-red-700 hover:text-white">Delete</button>
+                    </div>
+                </li>
             </ul>
         </div>
+        
         
     </AuthenticatedLayout>
 </template>
